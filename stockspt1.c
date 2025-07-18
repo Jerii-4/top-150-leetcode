34ee40ea68c0d32bae0e92c48be85b1568aa57fa
Jerii-4 <jerinbtc17@gmail.com>
2025-07-18 12:02:47 +0530

    new solutions

---
+++ stockspt1.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize) {
+    int buy=prices[0]; // initilase as 0 becuase we are assuming its the lowest prize
+    int profit= 0;
+    int i;
+    for(i=1; i<pricesSize;i++){
+        if(buy>prices[i]){ // we compare to get the smallest prize
+            buy=prices[i]; // update the smallest prize
+        }else if(prices[i]-buy>profit){ //checks for the maximum profit
+                profit=prices[i]-buy;  // update max profit
+            }
+        }
+        return profit;
+    }
--- unique1.c
+++ unique1.c
@@ -1,9 +1,9 @@
 //easy
 
 int removeDuplicates(int* nums, int numsSize) {
-    int i,k=1; //k=1 sincwe first inpuy will be uniwuw
+    int i,k=1; //k=1 sincwe first inpuy will be unique
     for (i=1;i<numsSize;i++){
-        if(nums[i]!=nums[j-1]){ // if condioton checks if the next elemnt is unique in the unique array
+        if(nums[i]!=nums[j-1]){ // if condition checks if the next elemnt is unique in the unique array
             nums[k]=nums[i];
             k++;
         }
